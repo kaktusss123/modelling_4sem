7c7be058a06aac8610be7d06463f57756f48f517
kaktusss123 <komarnitskij.v@phystech.edu>
2020-04-03 18:42:03 +0300

    initial commit

---
+++ kr1.py
@@ -0,0 +1,140 @@
+import numpy as np
+from numpy.linalg import matrix_power as mp
+
+def read_matrix(filename: str):
+    """
+    Читает матрицу из файла
+    """
+    matrix = np.zeros((15, 15))
+    with open(filename, encoding='utf-8') as f:
+        for i, line in enumerate(f):
+            matrix[i, :] = list(map(float, line.replace(',', '.').split()))
+    return matrix
+
+def task1(p: np.array, i: int, j: int, k: int) -> int:
+    """
+    Вероятность того, что за k шагов система перейдет из состояния i в состояние j
+    """
+    p_k = mp(p, k)
+    return p_k[i, j]
+
+def task2(p: np.array, a_0: np.array, k: int) -> np.array:
+    """
+    Вероятности состояний системы спустя k шагов, если в начальный момент вероятность состояний были a_0
+    """
+    return np.dot(mp(p, k), a_0)
+
+def task3(p: np.array, i_: int, j_: int, k: int) -> float:
+    """
+    Вероятность первого перехода за k шагов из состояния i в состояние j
+    """
+    p_ = p
+    for _ in range(2, k+1):  # 2, 3, 4, ..., k-1, k
+        new = np.zeros(p.shape)  # сделаем матрицу для рассчета вероятности первого прехода для конкретного k
+        # заполняем матрицу
+        for i in range(p.shape[0]):
+            for j in range(p.shape[1]):
+                s = 0
+                for m in range(p.shape[0]):
+                    s += p[i, m] * p_[m, j] if m != j else 0
+                new[i, j] = s
+        # делаем полученную матрицу p_(k-1)
+        p_ = new
+    return p_[i_, j_]
+
+def task4(p: np.array, i: int, j: int, k: int) -> float:
+    """
+    Вероятность перехода из i в j не позднее, чем за k шагов
+    """
+    s = 0
+    for t in range(1, k+1):
+        s += task3(p, i, j, t)
+    return s
+
+def task5(p: np.array, i: int, j: int) -> float:
+    """
+    Среднее количество шагов для перехода из состояния i в состояние j
+    """
+    t = 1
+    t_ = 0
+    for t in range(1, 10):  # поставить большое число
+        t_ += t * task3(p, i, j, t)
+    return t_
+
+
+def task6(p: np.array, j: int, k: int) -> float:
+    """
+    Вероятность первого возвращения в состояние j за k шагов
+    """
+    s = np.zeros(p.shape)
+    for m in range(1, k):
+        s += task6(p, j, m) * mp(p, k-m)
+    return (mp(p, k) - s)[j, j]
+
+
+def task7(p: np.array, j: int, k: int) -> float:
+    """
+    Вероятность возвращения в состояние j не позднее чем за k шагов
+    """
+    s = 0
+    for t in range(1, k+1):
+        s += task6(p, j, t)
+    return s
+
+def task8(p: np.array, j: int) -> float:
+    """
+    Среднее время возвращения в состояние j
+    """
+    s = 0
+    for t in range(1, 20):  # большое число
+        s += t * task6(p, j, t)
+    return s
+
+def task9(p):
+    """
+    Установившиеся вероятности
+    """
+    m = p.T - np.eye(p.shape[0])
+    m[-1, :] = 1
+    b = np.array([0] * (p.shape[0] - 1) + [1])
+    x = np.dot(np.linalg.inv(m), b)
+    return x
+
+if __name__ == "__main__":
+    matrix = read_matrix('matrix.csv')
+    
+    # ### Task1
+    # print(f'1) Вероятность того, что за 8 шагов система перейдет из состояния 11 в состояние 2: {task1(matrix, 10, 1, 8):.3f}')
+    
+    # ### Task2
+    # t2 = task2(matrix, np.array((0.08,0.11,0.09,0.11,0,0.01,0.05,0.01,0.12,0.05,0.07,0.11,0.06,0.05,0.08)), 10)
+    # print(f'2) Вероятности состояний системы спустя 10 шагов, если в начальный момент вероятность состояний были следующими: \nA=(0,08;0,11;0,09;0,11;0;0,01;0,05;0,01;0,12;0,05;0,07;0,11;0,06;0,05;0,08)\n')
+    # print(t2)
+
+    # ### Task3
+    # print(f'3) Вероятность первого перехода за 8 шагов из состояния 14 в состояние 3')
+    # print(task3(matrix, 13, 2, 8))
+
+    # ### Task4
+    # print(f'4) Вероятность перехода из состояния 3 в состояние 13 не позднее чем за 10 шагов')
+    # print(task4(matrix, 2, 12, 10))
+
+    ### Task5
+    # print(f'5) Среднее количество шагов для перехода из состояния 11 в состояние 15')
+    # print(task5(matrix, 10, 14))
+
+    # ### Task6
+    # print(f'6) Вероятность первого возвращения в состояние 5 за 7 шагов')
+    # print(task6(matrix, 4, 7))
+
+    ### Task7
+    print(f'7) Вероятность возвращения в состояние 5 не позднее чем за 6 шагов')
+    print(task7(matrix, 4, 6))
+
+    # ### Task8
+    # print(f'8) Среднее время возвращения в состояние 12')
+    # print(task8(matrix, 11))
+
+    # ### Task9
+    # print(f'Установившиеся вероятности')
+    # print(task9(matrix))
---
+++ kr2.py
@@ -0,0 +1,70 @@
+import numpy as np
+
+
+class SMO:
+
+    def __create_matrix(self):
+        self.__matrix = np.zeros((self.m + self.n + 1, self.m + self.n + 1))
+        for i in range(self.m + self.n):
+            self.__matrix[i, i+1] = self.lbd
+            self.__matrix[i+1, i] = self.mu * \
+                (i + 1) if i < self.m else self.mu * self.m
+
+    def __get_stable_prob(self):
+        """Установивишиеся вероятности"""
+        D = np.diag([self.__matrix[i, :].sum()
+                     for i in range(self.__matrix.shape[0])])
+        M = self.__matrix.T - D
+        M_ = M
+        M_[-1, :] = 1
+        B = np.zeros(M_.shape[0])
+        B[-1] = 1
+        self.__stable_prob = np.linalg.inv(M_).dot(B)
+
+    def __init__(self, lbd, m, mu, n):
+        self.lbd = lbd
+        self.m = m
+        self.mu = mu
+        self.n = n
+        self.__create_matrix()
+        self.__get_stable_prob()
+
+    # Ответы
+    matrix = property(lambda self: self.__matrix)
+    stable_prob = property(lambda self: self.__stable_prob)
+    p_otk = property(lambda self: self.__stable_prob[-1])
+    otn_prop_sp = property(lambda self: 1 - self.__stable_prob[-1])
+    abs_prop_sp = property(lambda self: (
+        1 - self.__stable_prob[-1]) * self.lbd)
+    L_o4 = property(lambda self: sum(
+        [i * self.__stable_prob[self.m + i] for i in range(1, self.n + 1)]))
+    T_o4 = property(lambda self: sum(
+        [(i + 1) / (self.m * self.mu) * self.__stable_prob[self.m + i] for i in range(self.n)]))
+    N_kan = property(lambda self: sum(
+        [i * self.__stable_prob[i] for i in range(1, self.m + 1)]))
+    p_no_o4 = property(lambda self: sum(self.__stable_prob[:self.m + 1]))
+    sr_t_prost = property(lambda self: self.__stable_prob[0] / self.mu)
+    sr_t_no_o4 = property(lambda self: sum([i + 1 / (self.m * self.lbd) * self.__stable_prob[i] for i in range(self.m)]))
+
+
+if __name__ == '__main__':
+    smo = SMO(29, 5, 6, 14)
+    # Task a - установившиеся вероятности
+    print('a) Установившиеся вероятности:', smo.stable_prob)
+    # Task b - вероятность отказа в обслуживании
+    print('b) Вероятность отказа в обслуживании:', smo.p_otk)
+    # Task c - Относительная и абсолютная интенсивность обслуживания
+    print('c) Относительная пропускная способность:', smo.otn_prop_sp)
+    print('c) Абсолютная пропускная способность:', smo.abs_prop_sp)
+    # Task d - Средняя длина в очереди
+    print('d) Средняя длина очереди:', smo.L_o4)
+    # Task e - Среднее время в очереди
+    print('e) Среднее время в очереди:', smo.T_o4)
+    # Task f - Среднее число занятых каналов
+    print('f) Среднее число занятых каналов:', smo.N_kan)
+    # Task g - вероятность того, что поступающая заявка не будет ждать в очереди
+    print('g) Вероятность того, что поступающая заявка не будет ждать в очереди:', smo.p_no_o4)
+    # Task h - Найти среднее время простоя системы массового обслужива
+    print('h) Среднее время простоя смо:', smo.sr_t_prost)
+    # Task i - Найти среднее время, когда в системе нет очереди.
+    print('i) Среднее время, когда в системе нет очереди:', smo.sr_t_no_o4)
